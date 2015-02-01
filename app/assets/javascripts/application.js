3581ffe5e7ce5cd9dc5ba5f784f943db82dc12f1
Sachin Singh <singhsachin87@yahoo.com>
2015-02-01 16:44:27 +0100

    application.js updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require turbolinks
-//= require ckeditor/override
 //= require ckeditor/init
 //= require moment
 //= require bootstrap-datetimepicker
