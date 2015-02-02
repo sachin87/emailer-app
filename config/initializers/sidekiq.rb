68e9bcdf2d52177236e90800caf9269f565eb109
Sachin Singh <singhsachin87@yahoo.com>
2015-02-02 06:27:53 +0100

    sidekiq initializer updated

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,24 +1,12 @@
-if Rails.env.production?
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['OPENREDIS_URL'] }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["OPENREDIS_URL"]}
-  end
-
-  ENV['REDIS_PROVIDER'] = ENV['OPENREDIS_URL'] || '127.0.0.1:6379'
-else
-  Sidekiq.configure_server do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'] }
-  end
-
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV["REDISTOGO_URL"]}
-  end
-
-  ENV['REDIS_PROVIDER'] = ENV['REDISTOGO_URL'] || '127.0.0.1:6379'
+Sidekiq.configure_server do |config|
+  config.redis = { url: ENV['REDISTOGO_URL'] }
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: ENV["REDISTOGO_URL"]}
+end
+
+ENV['REDIS_PROVIDER'] = ENV['REDISTOGO_URL'] || '127.0.0.1:6379'
 
 
 require 'sidekiq'
