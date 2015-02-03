3e61a92c4859cfd9216b4773214965c71e176a11
Sachin Singh <singhsachin87@yahoo.com>
2015-02-03 07:46:00 +0100

    bug fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
   end
 
   def scheduled_mails
-    @messages = Message.where(processed: false).order(:schedule_date)
+    @messages = Message.where(processed: false, enqueued: false).order(:schedule_date)
   end
 
   # GET /messages/1
